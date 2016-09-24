23a94914a4255999b69daa29d2c160da1c8a613e
Oleg <Oleg@Oleg-Laptop>
2016-09-24 11:44:48 +0300

    Changed number of employees in DB

--- cackler/src/main/resources/db/mysql/populateDB.sql
+++ cackler/src/main/resources/db/mysql/populateDB.sql
@@ -1,9 +1,12 @@
 INSERT IGNORE INTO specialties VALUES (1, 'HumanResources');
 INSERT IGNORE INTO specialties VALUES (2, 'Management');
-INSERT IGNORE INTO specialties VALUES (3, 'Accauntant');
+INSERT IGNORE INTO specialties VALUES (3, 'Accountant');
 
 INSERT IGNORE INTO owners VALUES (1, 'George', 'Franklin', '1980-09-07', 5000, 1);
 INSERT IGNORE INTO owners VALUES (2, 'Betty', 'Davis', '1982-03-09', 4900, 3);
 INSERT IGNORE INTO owners VALUES (3, 'Eduardo', 'Rodriquez', '1967-11-01',3700 ,2);
 INSERT IGNORE INTO owners VALUES (4, 'Harold', 'Davis', '1978-05-09', 4500, 2);
 INSERT IGNORE INTO owners VALUES (5, 'Peter', 'McTavish', '1987-06-09', 5900, 3);
+INSERT IGNORE INTO owners VALUES (6, 'John', 'Pew', '1956-01-01', 5100, 3);
+INSERT IGNORE INTO owners VALUES (7, 'Hew', 'Laury', '1966-11-09', 2900, 1);
+INSERT IGNORE INTO owners VALUES (8, 'Diana', 'Crowly', '1992-02-05', 4700, 3);
