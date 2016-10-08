ce8e4cb0092003ef24c75eb87943624be53263e0
irukanji74 <amilliarder@gmail.com>
2016-10-08 17:27:47 +0500

    Using org.springframwork.orm version 3.2.0. solved problem

--- cackler/src/main/java/repositories/jdbc/JdbcDepartmentRepository.java
+++ cackler/src/main/java/repositories/jdbc/JdbcDepartmentRepository.java
@@ -18,7 +18,7 @@ import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
-//import org.springframework.orm.ObjectRetrievalFailureException;
+import org.springframework.orm.ObjectRetrievalFailureException;
 import org.springframework.stereotype.Repository;
 
 import model.Department;
